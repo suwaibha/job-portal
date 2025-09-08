57ea3398fc0bcc200f472ed5fdaf245afd0dc5ae
suwaibha <suwaibharauf08@gmail.com>
2025-09-08 12:48:25 +0530

    final i guess

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
 
     mongoose.connection.on('connected',()=>console.log('Database connected'))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 
 }
 
