37e7f2396f1d6d72674b4a4487ac077ab399190e
suwaibha <suwaibharauf08@gmail.com>
2025-09-08 12:41:13 +0530

    final

--- server/server.js
+++ server/server.js
@@ -44,14 +44,6 @@ import clerkwebhooks from './controllers/webhooks.js'
 // Initialize Express
 const app = express()
 
-let isConnected = false
-async function initDB() {
-  if (!isConnected) {
-    await connectDB()
-    isConnected = true
-  }
-}
-
 // Connect to database
 await connectDB()
 
